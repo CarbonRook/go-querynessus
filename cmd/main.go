74a7d413a51506a08979548a61ecadaa5945c957
CarbonRook <28928135+CarbonRook@users.noreply.github.com>
2021-09-01 12:13:58 +0100

    Updated main with flags

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
-	"go-querynessus/querynessus"
+	"flag"
+	"fmt"
 	"log"
 	"os"
+
+	"github.com/carbonrook/go-querynessus/querynessus"
 )
 
+var TENABLE_ACCESS_KEY = "TENABLE_ACCESS_KEY"
+var TENABLE_SECRET_KEY = "TENABLE_SECRET_KEY"
+
 func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+
+		flag.PrintDefaults()
+
+		fmt.Fprintf(os.Stderr, "\nRequired environment vars:\n%s: Tenable API access key\n%s: Tenable API secret key\n", TENABLE_ACCESS_KEY, TENABLE_SECRET_KEY)
+	}
+	outfileArg := flag.String("out", "nessus-plugins.json", "The file to output the JSON to")
+	flag.Parse()
+
 	creds := querynessus.TenableCredentials{
-		AccessKey: os.Getenv("TENABLE_ACCESS_KEY"),
-		SecretKey: os.Getenv("TENABLE_SECRET_KEY"),
+		AccessKey: os.Getenv(TENABLE_ACCESS_KEY),
+		SecretKey: os.Getenv(TENABLE_SECRET_KEY),
 	}
 
 	params := querynessus.RequestParams{
@@ -32,5 +48,5 @@ func main() {
 		Size: len(results),
 	}
 
-	combinedPage.SaveToFile("plugins.json")
+	combinedPage.SaveToFile(*outfileArg)
 }
